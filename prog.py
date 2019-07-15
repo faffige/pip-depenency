3b5dd75232c4b9f26001bc08921d2e519dd2625c
faffige <29091666+faffige@users.noreply.github.com>
2019-07-15 05:09:12 -0700

    first draft of dependency program

---
+++ prog.py
@@ -0,0 +1,59 @@
+import os, random, string
+from pathlib import Path
+
+#create a new random dir
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+#Get the current directory
+dirpath = os.getcwd()
+#Get the parent directory
+parentDir = Path(dirpath).parent
+#Generate a random string for a folder name
+folderName = randomString()
+#Build string of the new path
+newDir = "{}/{}".format(parentDir, folderName)
+
+print(newDir)
+
+# Create target Directory if don't exist
+if not os.path.exists(newDir):
+  os.mkdir(newDir)
+  print("Directory " , newDir ,  " Created ")
+else:    
+  print("Directory " , newDir ,  " already exists")
+
+
+print(os.getcwd())
+os.chdir(newDir)
+os.system('pwd')
+
+venv = 'virtualenv venv'
+pipInstall = 'pip install requests'
+freeze = 'pip freeze > requirements.txt'
+
+os.system(venv)
+activate_this_file = "{}/venv/bin/activate_this.py".format(newDir)
+
+
+with open(activate_this_file) as f:
+    code = compile(f.read(), activate_this_file, 'exec')
+    exec(code)
+
+os.system(pipInstall)
+os.system(freeze)
+os.system('deactivate')
+
+f=open("requirements.txt", "r")
+if f.mode == 'r':
+    contents =f.read()
+    print(contents)
+
+# remove target directory if it exists
+if os.path.exists(newDir):
+  os.rmdir(newDir)
+  print("Directory " , newDir ,  " removed ")
+else:    
+  print("Directory " , newDir ,  " does not exist")
